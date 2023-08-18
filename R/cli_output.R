011070c1e5460c67b9582b5ba49c84166f16534a
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-08-18 19:55:32 -0400

    revise with styler

--- R/cli_output.R
+++ R/cli_output.R
@@ -8,7 +8,7 @@ checkthat_cli_theme <- function(variables) {
   theme
 }
 
-encouraging_message <- function(){
+encouraging_message <- function() {
   encouraging_messages <- c(
     "You've got this!",
     "Believe in yourself",
@@ -22,20 +22,21 @@ encouraging_message <- function(){
     "Hard work pays off",
     "Stay positive, stay persistent",
     "Perseverance leads to success",
-    "Small victories count")
+    "Small victories count"
+  )
 
   chosen_message <- sample(encouraging_messages, size = 1)
 
   chosen_message
 }
 
-cli_check_success <- function(){
+cli_check_success <- function() {
   cli::start_app(theme = checkthat_cli_theme())
   cli::cli_alert_success("all data checks passing")
 }
 
-cli_add_encouragement <- function(msg = encouraging_message(), p = .50){
-  if(runif(1) < p){
+cli_add_encouragement <- function(msg = encouraging_message(), p = .33) {
+  if (runif(1) < p) {
     cli::start_app(theme = checkthat_cli_theme())
     cli::cli_text("")
     cli::cli_text(msg)
@@ -44,7 +45,7 @@ cli_add_encouragement <- function(msg = encouraging_message(), p = .50){
   invisible(msg)
 }
 
-cli_throw_test_error <- function(){
+cli_throw_test_error <- function() {
   cli::start_app(theme = checkthat_cli_theme())
   cli::cli_text("")
   cli::cli_text("")
@@ -52,11 +53,12 @@ cli_throw_test_error <- function(){
     paste(
       "At least one data check failed. Check your tests before moving on.",
       "Note, if needed, you can prevent check_that from halting execution by",
-      "setting {.code raise_error = FALSE}."))
+      "setting {.code raise_error = FALSE}."
+    )
+  )
 }
 
 cli_check_summary <- function(test_results, test_labs) {
-
   assertthat::are_equal(length(test_results), length(test_labs))
 
   cli::start_app(theme = checkthat_cli_theme())
@@ -66,9 +68,8 @@ cli_check_summary <- function(test_results, test_labs) {
   for (i in seq_along(test_results)) {
     result <- "{test_labs[i]} --> {test_results[i]}"
 
-    if(test_results[i]){
+    if (test_results[i]) {
       cli::cli_alert_success(result)
-
     } else {
       cli::cli_alert_danger(result)
     }
@@ -82,14 +83,13 @@ cli_check_summary <- function(test_results, test_labs) {
   invisible(check_summary)
 }
 
-cli_print_checks <- function(test_results, test_labs, encourage = TRUE){
-  if(all(test_results) == TRUE){
+cli_print_checks <- function(test_results, test_labs, encourage = TRUE) {
+  if (all(test_results) == TRUE) {
     cli_check_success()
-
   } else {
     cli_check_summary(test_results, test_labs)
 
-    if(encourage){
+    if (encourage) {
       cli_add_encouragement()
     }
   }
