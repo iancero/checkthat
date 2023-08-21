843e1535906b40901ca601aed5d4880c8e3ec894
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-08-21 10:15:55 -0400

    add initial support for .d

--- R/check_that.R
+++ R/check_that.R
@@ -1,8 +1,14 @@
 check_that <- function(.data, ..., print = TRUE, raise_error = TRUE, encourage = TRUE){
+
+  # TODO: add support for grouped data frames
+
   dots <- rlang::enquos(...)
 
+  data_mask <- rlang::as_data_mask(.data)
+  data_mask$.d <- .data
+
   test_labs <- purrr::map_chr(dots, .f = rlang::as_label)
-  test_results <- purrr::map_lgl(dots, .f = ~ rlang::eval_tidy(.x, .data))
+  test_results <- purrr::map_lgl(dots, .f = ~ rlang::eval_tidy(.x, data_mask))
 
   if(print){
     cli_print_checks(test_results, test_labs, encourage = encourage)
@@ -14,3 +20,4 @@ check_that <- function(.data, ..., print = TRUE, raise_error = TRUE, encourage =
 
   invisible(.data)
 }
+
