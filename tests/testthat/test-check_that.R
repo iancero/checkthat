2ca0d8b2d81b45b88f6eb7ed6ca0091e5939a9fb
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-08-21 10:16:48 -0400

    add initial tests

--- tests/testthat/test-check_that.R
+++ tests/testthat/test-check_that.R
@@ -1,3 +1,146 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+sample_data <- data.frame(
+  x = c(1, 2, 3),
+  y = c("A", "B", "C")
+)
+
+test_that("Column 'x' exists", {
+  expect_no_error(check_that(sample_data, exists('x')))
+})
+
+test_that("Column 'z' does not exist", {
+  expect_error(check_that(sample_data, exists('z')))
+})
+
+test_that("Mean of column 'x' is 2", {
+  expect_no_error(check_that(sample_data, mean(x) == 2))
+})
+
+test_that("Number of rows is greater than 0", {
+  expect_no_error(check_that(sample_data, nrow(.d) > 0))
+})
+
+test_that("Column 'y' contains unique values", {
+  expect_no_error(check_that(sample_data, length(unique(y)) == nrow(.d)))
+})
+
+test_that("Data frame has 2 columns", {
+  expect_no_error(check_that(sample_data, ncol(.d) == 2))
+})
+
+test_that("Column 'y' contains valid values", {
+  expect_no_error(check_that(sample_data, all(y %in% c("A", "B", "C"))))
+})
+
+test_that("Column 'x' has numeric values", {
+  expect_no_error(check_that(sample_data, all(is.numeric(x))))
+})
+
+test_that("Column 'x' has no missing values", {
+  expect_no_error(check_that(sample_data, all(!is.na(x))))
+})
+
+test_that("Column 'y' has character values", {
+  expect_no_error(check_that(sample_data, all(is.character(y))))
+})
+
+test_that("Column 'x' has values > 0", {
+  expect_no_error(check_that(sample_data, all(x > 0)))
+})
+
+test_that("Column 'y' has values of length 1", {
+  expect_no_error(check_that(sample_data, all(nchar(y) == 1)))
+})
+
+test_that("Data frame has specific column names", {
+  expect_no_error(check_that(sample_data, all(c("x", "y") %in% colnames(.d))))
+})
+
+test_that("Column 'x' has all unique values", {
+  expect_no_error(check_that(sample_data, length(unique(x)) == length(x)))
+})
+
+test_that("Column 'x' has valid levels", {
+  expect_no_error(check_that(data.frame(factor_col = factor(c("A", "B", "C"))),
+                             all(factor_col %in% c("A", "B", "C"))))
+})
+
+test_that("Date column is within range", {
+  expect_no_error(check_that(data.frame(date_col = as.Date(c("2023-01-01", "2023-06-30"))),
+                             all(date_col >= as.Date("2023-01-01") & date_col <= as.Date("2023-12-31"))))
+})
+
+test_that("Time column has valid values", {
+  expect_no_error(
+    check_that(
+      data.frame(
+        time_col = as.POSIXct(c("2023-08-19 12:00:00", "2023-08-19 15:30:00"))),
+       all(lubridate::hour(time_col) %in% c(12, 15) & lubridate::minute(time_col) %in% c(0, 30))))
+})
+
+test_that("Function works within a pipeline", {
+  expect_no_error(
+    sample_data |>
+      dplyr::filter(x > 1) |>
+      check_that(mean(x) > 1))
+})
+
+# test_that("Function works with grouped data", {
+#
+#   # TODO: add support for grouped data frames
+#
+#   expect_no_error(check_that(sample_data |> dplyr::group_by(y) |> check_that(n() == 1)))
+# })
+
+test_that("Function works with a mutate operation", {
+  expect_no_error(
+    sample_data |>
+      dplyr::mutate(new_col = x * 2) |>
+      check_that(all(new_col == x * 2)))
+})
+
+test_that("Function works with summarise operation", {
+  expect_no_error(
+    sample_data |>
+      dplyr::summarise(mean_x = mean(x)) |>
+      check_that(mean_x > 0))
+})
+
+test_that("Function works with left_join operation", {
+  expect_no_error(
+    sample_data |>
+      dplyr::left_join(
+          y = data.frame(
+            y = c("A", "C", "B"),
+            z = c("apple", "banana", "clementine")),
+          by = "y") |>
+       check_that(all(!is.na(z))))
+})
+
+test_that("Function works with across operation", {
+  expect_no_error(
+    sample_data |>
+      dplyr::mutate(dplyr::across(.cols = x, .fns = ~ .x * 10)) |>
+      check_that(all(x >= 10)))
+})
+
+test_that("Function works with arrange operation", {
+  expect_no_error(
+    sample_data |>
+      dplyr::arrange(desc(x)) |>
+      check_that(x[1] >= x[2] && x[2] >= x[3]))
+})
+
+test_that("Function works with filter operation", {
+  expect_no_error(
+    sample_data |>
+      dplyr::filter(x > 1) |>
+      check_that(all(x > 1)))
+})
+
+test_that("Function works with nested data frames", {
+  expect_no_error(sample_data |>
+                    tidyr::nest() |>
+                    dplyr::mutate(data = purrr::map(data, ~ check_that(.x, all(x <= 3)))) |>
+                    tidyr::unnest(data) |>
+                    check_that(all(x <= 3)))
 })
---
+++ tests/testthat/test-utils-typechecks.R
@@ -0,0 +1,126 @@
+test_that("is_integerlike correctly identifies integer-like values", {
+  expect_true(is_integerlike(5))
+  expect_true(is_integerlike(5L))
+  expect_true(is_integerlike(-5.0))
+  expect_true(is_integerlike(0))
+  expect_true(is_integerlike(-10))
+  expect_true(is_integerlike(1000))
+})
+
+test_that("is_integerlike correctly identifies non-integer-like values", {
+  expect_false(is_integerlike(5.5))
+  expect_false(is_integerlike(1.23))
+  expect_false(is_integerlike(-1.23))
+  expect_false(is_integerlike("string"))
+  expect_false(is_integerlike(c(1, 2, 3)))
+})
+
+test_that("is_proportion correctly identifies valid proportion values", {
+  expect_true(is_proportion(0))
+  expect_true(is_proportion(0.5))
+  expect_true(is_proportion(0.75))
+  expect_true(is_proportion(1))
+  expect_true(is_proportion(1L))
+})
+
+test_that("is_proportion correctly identifies non-proportion values", {
+  expect_false(is_proportion(1.5))
+  expect_false(is_proportion(-0.2))
+  expect_false(is_proportion(2))
+  expect_false(is_proportion("string"))
+  expect_false(is_proportion(c(0.2, 0.3)))
+})
+
+test_that("validate_proportion correctly validates valid proportion values", {
+  expect_no_error(validate_proportion(0))
+  expect_no_error(validate_proportion(0.5))
+  expect_no_error(validate_proportion(0.75))
+  expect_no_error(validate_proportion(1))
+})
+
+test_that("validate_proportion correctly throws an error for non-proportion values", {
+  expect_error(validate_proportion(1.5), regexp = "Not a valid proportion")
+  expect_error(validate_proportion(-0.2), regexp = "Not a valid proportion")
+  expect_error(validate_proportion(2), regexp = "Not a valid proportion")
+  expect_error(validate_proportion("string"), regexp = "Not a valid proportion")
+  expect_error(validate_proportion(c(0.2, 0.3)), regexp = "Not a valid proportion")
+})
+
+test_that("is_count correctly identifies valid count values (including zero)", {
+  expect_true(is_count(0))
+  expect_true(is_count(5))
+  expect_true(is_count(10))
+})
+
+test_that("is_count correctly identifies non-count values (including zero)", {
+  expect_false(is_count(5.5))
+  expect_false(is_count(-10))
+  expect_false(is_count("string"))
+  expect_false(is_count(c(1, 2, 3)))
+})
+
+test_that("is_count correctly identifies valid count values (excluding zero)", {
+  expect_true(is_count(1, include_zero = FALSE))
+  expect_true(is_count(5, include_zero = FALSE))
+  expect_true(is_count(10, include_zero = FALSE))
+})
+
+test_that("is_count correctly identifies non-count values (excluding zero)", {
+  expect_false(is_count(0, include_zero = FALSE))
+  expect_false(is_count(5.5, include_zero = FALSE))
+  expect_false(is_count(-10, include_zero = FALSE))
+  expect_false(is_count("string", include_zero = FALSE))
+  expect_false(is_count(c(1, 2, 3), include_zero = FALSE))
+})
+
+test_that("validate_count correctly validates valid count values (including zero)", {
+  expect_no_error(validate_count(0))
+  expect_no_error(validate_count(5))
+  expect_no_error(validate_count(10))
+})
+
+test_that("validate_count correctly throws an error for non-count values (including zero)", {
+  expect_error(validate_count(5.5), regexp = "Not a valid count value")
+  expect_error(validate_count(-10), regexp = "Not a valid count value")
+  expect_error(validate_count("string"), regexp = "Not a valid count value")
+  expect_error(validate_count(c(1, 2, 3)), regexp = "Not a valid count value")
+})
+
+test_that("validate_count correctly validates valid count values (excluding zero)", {
+  expect_no_error(validate_count(1, include_zero = FALSE))
+  expect_no_error(validate_count(5, include_zero = FALSE))
+  expect_no_error(validate_count(10, include_zero = FALSE))
+})
+
+test_that("validate_count correctly throws an error for non-count values (excluding zero)", {
+  expect_error(validate_count(0, include_zero = FALSE), regexp = "Not a valid count value")
+  expect_error(validate_count(5.5, include_zero = FALSE), regexp = "Not a valid count value")
+  expect_error(validate_count(-10, include_zero = FALSE), regexp = "Not a valid count value")
+  expect_error(validate_count("string", include_zero = FALSE), regexp = "Not a valid count value")
+  expect_error(validate_count(c(1, 2, 3), include_zero = FALSE), regexp = "Not a valid count value")
+})
+
+test_that("validate_logical_vec correctly validates a valid logical vector", {
+  expect_no_error(validate_logical_vec(c(TRUE, FALSE, TRUE)))
+  expect_no_error(validate_logical_vec(c(FALSE, TRUE, FALSE)))
+  expect_no_error(validate_logical_vec(c(TRUE, TRUE, TRUE)))
+  expect_no_error(validate_logical_vec(c(FALSE, FALSE, FALSE)))
+})
+
+test_that("validate_logical_vec correctly throws an error for non-logical vectors", {
+  expect_error(validate_logical_vec(c(1, 0, 1)), regexp = "logical_vec must be a logical vector")
+  expect_error(validate_logical_vec(c("TRUE", "FALSE")), regexp = "logical_vec must be a logical vector")
+})
+
+test_that("validate_logical_vec correctly throws an error for empty vectors", {
+  expect_error(validate_logical_vec(integer(0)), regexp = "logical_vec must have length of at least 1")
+  expect_error(validate_logical_vec(logical(0)), regexp = "logical_vec must have length of at least 1")
+  expect_error(validate_logical_vec(character(0)), regexp = "logical_vec must have length of at least 1")
+})
+
+test_that("validate_logical_vec correctly warns for all NA values", {
+  expect_warning(validate_logical_vec(NA), regexp = "All values of logical_vec are NA")
+  expect_warning(validate_logical_vec(c(NA, NA, NA)), regexp = "All values of logical_vec are NA")
+})
+
+
