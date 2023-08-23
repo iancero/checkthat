961ab5e00bd3fe983e408168c3e91c9ec8c38bfa
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-08-22 23:15:32 -0400

    add initial tests

--- tests/testthat/test-cli_output.R
+++ tests/testthat/test-cli_output.R
@@ -1,3 +1,31 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("checkthat_cli_theme returns a modified CLI theme", {
+  variables <- c("h1", ".alert-success", ".alert-danger", "body")
+  theme <- checkthat_cli_theme(variables)
+
+  expect_equal(theme[["h1"]][["color"]], "grey40")
+  expect_equal(theme[[".alert-success"]][["color"]], "grey40")
+  expect_equal(theme[[".alert-danger"]][["color"]], "grey40")
+  expect_equal(theme[["body"]][["color"]], "grey40")
+})
+
+test_that("encouraging_message returns a message from the list", {
+  encouraging_messages <- c(
+    "You've got this!",
+    "Believe in yourself",
+    "Keep pushing forward",
+    "Every step is progress",
+    "Stay determined, success is near",
+    "Challenges help you grow",
+    "You're making it happen!",
+    "Keep your focus, you'll get there",
+    "Trust the process",
+    "Hard work pays off",
+    "Stay positive, stay persistent",
+    "Perseverance leads to success",
+    "Small victories count"
+  )
+
+  message <- encouraging_message()
+
+  expect_true(message %in% encouraging_messages)
 })
