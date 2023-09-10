0eb9450fb4091acdd3a84a5fe5521a08ae441b11
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-09-10 17:24:05 -0400

    add documentation for internal cli functions

--- R/cli_output.R
+++ R/cli_output.R
@@ -1,4 +1,13 @@
-checkthat_cli_theme <- function(variables) {
+#' Internal Function to Customize the CLI Theme for checkthat Messages
+#'
+#' This is an internal function that is not intended for external use. It
+#' customizes the Command Line Interface (CLI) theme for messages displayed
+#' by the checkthat package.
+#'
+#' @noRd
+#'
+#' @returns A customized CLI theme for checkthat messages.
+checkthat_cli_theme <- function() {
   theme <- cli::builtin_theme()
   theme[["h1"]][["color"]] <- "grey40"
   theme[[".alert-success"]][["color"]] <- "grey40"
@@ -8,6 +17,14 @@ checkthat_cli_theme <- function(variables) {
   theme
 }
 
+#' Internal Function to Retrieve a Random Encouraging Message
+#'
+#' This is an internal function that is not intended for external use.
+#' It retrieves a random encouraging message from a predefined list of messages.
+#'
+#' @noRd
+#'
+#' @returns A randomly selected string containing an encouraging message.
 encouraging_message <- function() {
   encouraging_messages <- c(
     "You've got this!",
@@ -30,11 +47,30 @@ encouraging_message <- function() {
   chosen_message
 }
 
+#' Internal Function to Display a Success Message in the CLI
+#'
+#' This is an internal function that is not intended for external use. It
+#' displays a success message in the Command Line Interface (CLI) using the
+#' customized checkthat theme.
+#'
+#' @noRd
 cli_check_success <- function() {
   cli::start_app(theme = checkthat_cli_theme())
   cli::cli_alert_success("all data checks passing")
 }
 
+#' Internal Function to Add Encouraging Messages to the CLI
+#'
+#' This is an internal function that is not intended for external use. It adds
+#' encouraging messages to the Command Line Interface (CLI) using the customized
+#' checkthat theme.
+#'
+#' @param msg A custom message to display. If not provided, a random encouraging message will be selected.
+#' @param p The probability of displaying the message. Default is 0.33.
+#'
+#' @noRd
+#'
+#' @returns (invisibly) The unmodifiied \code{msg} string from above.
 cli_add_encouragement <- function(msg = encouraging_message(), p = .33) {
   if (stats::runif(1) < p) {
     cli::start_app(theme = checkthat_cli_theme())
@@ -45,6 +81,13 @@ cli_add_encouragement <- function(msg = encouraging_message(), p = .33) {
   invisible(msg)
 }
 
+#' Internal Function to Trigger a Test Error in the CLI
+#'
+#' This is an internal function that is not intended for external use. It
+#' triggers a test error message in the Command Line Interface (CLI) using the
+#' customized checkthat theme.
+#'
+#' @noRd
 cli_throw_test_error <- function() {
   cli::start_app(theme = checkthat_cli_theme())
   cli::cli_text("")
@@ -52,6 +95,18 @@ cli_throw_test_error <- function() {
   cli::cli_abort("At least one data check failed.")
 }
 
+#' Internal Function to Display a Summary of Test Results in the CLI
+#'
+#' This is an internal function that is not intended for external use. It
+#' displays a summary of test results in the Command Line Interface (CLI) using
+#' the customized checkthat theme.
+#'
+#' @param test_results A logical vector representing the results of data checks.
+#' @param test_labs A character vector containing labels for the data checks.
+#'
+#' @noRd
+#'
+#' @returns (invisibly) A named character vector of check results.
 cli_check_summary <- function(test_results, test_labs) {
   if (length(test_results) != length(test_labs)){
     stop("length(test_results) != length(test_labs)")
@@ -79,6 +134,18 @@ cli_check_summary <- function(test_results, test_labs) {
   invisible(check_summary)
 }
 
+#' Internal Function to Print Data Check Results in the CLI
+#'
+#' This is an internal function that is not intended for external use. It prints
+#' the results of data checks in the Command Line Interface (CLI) using the
+#' customized checkthat theme.
+#'
+#' @param test_results A logical vector representing the results of data checks.
+#' @param test_labs A character vector containing labels for the data checks.
+#' @param encourage Logical. If \code{TRUE}, encouraging messages will be
+#'                  displayed when tests pass.
+#'
+#' @noRd
 cli_print_checks <- function(test_results, test_labs, encourage = TRUE) {
   if (all(test_results) == TRUE) {
     cli_check_success()
@@ -90,3 +157,4 @@ cli_print_checks <- function(test_results, test_labs, encourage = TRUE) {
     }
   }
 }
+
