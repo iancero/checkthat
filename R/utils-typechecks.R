d07eab51147d078f63c17c5de1e521f71ec179c6
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-09-10 18:16:48 -0400

    wrap error-producing examples in try()

--- R/utils-typechecks.R
+++ R/utils-typechecks.R
@@ -4,7 +4,7 @@
 #' it is numeric and its length is 1.
 #'
 #' @param x Numeric value to check.
-#' @return \code{TRUE} if \code{x} is integer-like, otherwise \code{FALSE}.
+#' @returns \code{TRUE} if \code{x} is integer-like, otherwise \code{FALSE}.
 #'
 #' @seealso \code{\link{is_proportion}}, \code{\link{is_count}},
 #'          \code{\link{validate_proportion}}, \code{\link{validate_count}}
@@ -14,7 +14,7 @@
 #' is_integerlike(3.5)    # FALSE
 #' is_integerlike("3")    # FALSE
 #' is_integerlike(c(1,2)) # FALSE
-#'
+#' @export
 is_integerlike <- function(x) {
   if (!is.numeric(x)) {
     return(FALSE)
@@ -33,7 +33,7 @@ is_integerlike <- function(x) {
 #' is numeric and within the range of 0 to 1 (inclusive).
 #'
 #' @param x Numeric value to check.
-#' @return \code{TRUE} if \code{x} is a proportion, otherwise \code{FALSE}.
+#' @returns \code{TRUE} if \code{x} is a proportion, otherwise \code{FALSE}.
 #'
 #' @seealso \code{\link{is_integerlike}}, \code{\link{is_count}},
 #'          \code{\link{validate_proportion}}, \code{\link{validate_count}}
@@ -42,7 +42,7 @@ is_integerlike <- function(x) {
 #' is_proportion(0.5)  # TRUE
 #' is_proportion(1.2)  # FALSE
 #' is_proportion(-0.2) # FALSE
-#'
+#' @export
 is_proportion <- function(x) {
   if (!is.numeric(x)) {
     return(FALSE)
@@ -61,7 +61,7 @@ is_proportion <- function(x) {
 #' (ranging from 0 to 1, inclusive).
 #'
 #' @param x Numeric value to validate as a proportion.
-#' @return \code{TRUE} if \code{x} is a valid proportion, otherwise it throws
+#' @returns \code{TRUE} if \code{x} is a valid proportion, otherwise it throws
 #'          an error.
 #'
 #' @seealso \code{\link{is_proportion}}, \code{\link{is_count}},
@@ -69,8 +69,8 @@ is_proportion <- function(x) {
 #'
 #' @examples
 #' validate_proportion(0.5)   # TRUE
-#' validate_proportion(1.2)   # Error: Not a valid proportion
-#'
+#' try(validate_proportion(1.2))   # Error
+#' @export
 validate_proportion <- function(x) {
   if (!is_proportion(x)) {
     stop("Not a valid proportion")
@@ -86,7 +86,7 @@ validate_proportion <- function(x) {
 #'
 #' @param x Numeric value to check.
 #' @param include_zero Logical, whether to include zero as a valid count.
-#' @return \code{TRUE} if \code{x} is a count, otherwise \code{FALSE}.
+#' @returns \code{TRUE} if \code{x} is a count, otherwise \code{FALSE}.
 #'
 #' @seealso \code{\link{is_proportion}}, \code{\link{is_integerlike}},
 #'          \code{\link{validate_count}}, \code{\link{validate_proportion}}
@@ -97,7 +97,7 @@ validate_proportion <- function(x) {
 #' is_count(0, include_zero = FALSE) # FALSE
 #' is_count(-1)  # FALSE
 #' is_count(1.5) # FALSE
-#'
+#' @export
 is_count <- function(x, include_zero = TRUE) {
   if (!is_integerlike(x)) {
     return(FALSE)
@@ -117,7 +117,7 @@ is_count <- function(x, include_zero = TRUE) {
 #'
 #' @param x Numeric value to validate as a count.
 #' @param include_zero Logical, whether to include zero as a valid count.
-#' @return \code{TRUE} if \code{x} is a valid count, otherwise it throws an
+#' @returns \code{TRUE} if \code{x} is a valid count, otherwise it throws an
 #'          error.
 #'
 #' @seealso \code{\link{is_count}}, \code{\link{is_proportion}},
@@ -126,9 +126,9 @@ is_count <- function(x, include_zero = TRUE) {
 #' @examples
 #' validate_count(0)   # TRUE
 #' validate_count(3)   # TRUE
-#' validate_count(0, include_zero = FALSE) # Error: Not a valid count value.
-#' validate_count(-1)  # Error: Not a valid count value.
-#'
+#' try(validate_count(0, include_zero = FALSE)) # Error: Not a valid count
+#' try(validate_count(-1))  # Error: Not a valid count value.
+#' @export
 validate_count <- function(x, include_zero = TRUE) {
   if (!is_count(x, include_zero = include_zero)) {
     stop("Not a valid count value (i.e., integer of zero or greater).")
@@ -146,7 +146,7 @@ validate_count <- function(x, include_zero = TRUE) {
 #' - If all values are NA, it will raise a warning.
 #'
 #' @param logical_vec Logical vector to validate.
-#' @return \code{TRUE} if the logical vector is valid, otherwise it throws an
+#' @returns \code{TRUE} if the logical vector is valid, otherwise it throws an
 #'          error.
 #'
 #' @seealso \code{\link{is_proportion}}, \code{\link{is_count}},
@@ -154,9 +154,9 @@ validate_count <- function(x, include_zero = TRUE) {
 #'
 #' @examples
 #' validate_logical_vec(c(TRUE, FALSE, TRUE)) # TRUE
-#' validate_logical_vec(c()) # Error: logical_vec must have length of at least 1
-#' validate_logical_vec(c(NA, NA)) # Warning: All values of logical_vec are NA
-#'
+#' try(validate_logical_vec(c())) # Error
+#' validate_logical_vec(c(NA, NA)) # Warning
+#' @export
 validate_logical_vec <- function(logical_vec) {
   if (length(logical_vec) < 1) {
     stop("logical_vec must have length of at least 1")
